4677cca7a646b8f0193a8327019fe01ce346e7a8
Anmol Sharan Nagi <muzik999@gra-login1.graham.sharcnet>
2020-10-26 16:19:33 -0400

    fixed failing test(epoch)

--- train_mtl.py
+++ train_mtl.py
@@ -297,128 +297,129 @@ def train(epoch):
 
 
 def test(epoch):
-    global best_accuracy
-    global best_miou
-    model.eval()
-    test_loss_iou = 0
-    test_loss_vec = 0
-    hist = np.zeros((config["task1_classes"], config["task1_classes"]))
-    hist_angles = np.zeros((config["task2_classes"], config["task2_classes"]))
-    crop_size = config["val_dataset"][args.dataset]["crop_size"]
-    for i, (inputsBGR, labels, vecmap_angles) in enumerate(val_loader, 0):
-        inputsBGR = Variable(
-            inputsBGR.float().cuda(), volatile=True, requires_grad=False
-        )
+    with torch.no_grad():
+        global best_accuracy
+        global best_miou
+        model.eval()
+        test_loss_iou = 0
+        test_loss_vec = 0
+        hist = np.zeros((config["task1_classes"], config["task1_classes"]))
+        hist_angles = np.zeros((config["task2_classes"], config["task2_classes"]))
+        crop_size = config["val_dataset"][args.dataset]["crop_size"]
+        for i, (inputsBGR, labels, vecmap_angles) in enumerate(val_loader, 0):
+            inputsBGR = Variable(
+                inputsBGR.float().cuda(), volatile=True, requires_grad=False
+            )
 
-        outputs, pred_vecmaps = model(inputsBGR)
-        if args.multi_scale_pred:
-            loss1 = road_loss(outputs[0], util.to_variable(labels[0], True, False), True)
-            num_stacks = model.module.num_stacks if num_gpus > 1 else model.num_stacks
-            for idx in range(num_stacks - 1):
-                loss1 += road_loss(outputs[idx + 1], util.to_variable(labels[0], True, False), True)
-            for idx, output in enumerate(outputs[-2:]):
-                loss1 += road_loss(output, util.to_variable(labels[idx + 1], True, False), True)
+            outputs, pred_vecmaps = model(inputsBGR)
+            if args.multi_scale_pred:
+                loss1 = road_loss(outputs[0], util.to_variable(labels[0], True, False), True)
+                num_stacks = model.module.num_stacks if num_gpus > 1 else model.num_stacks
+                for idx in range(num_stacks - 1):
+                    loss1 += road_loss(outputs[idx + 1], util.to_variable(labels[0], True, False), True)
+                for idx, output in enumerate(outputs[-2:]):
+                    loss1 += road_loss(output, util.to_variable(labels[idx + 1], True, False), True)
 
-            loss2 = angle_loss(pred_vecmaps[0], util.to_variable(vecmap_angles[0], True, False))
-            for idx in range(num_stacks - 1):
-                loss2 += angle_loss(
-                    pred_vecmaps[idx + 1], util.to_variable(vecmap_angles[0], True, False)
+                loss2 = angle_loss(pred_vecmaps[0], util.to_variable(vecmap_angles[0], True, False))
+                for idx in range(num_stacks - 1):
+                    loss2 += angle_loss(
+                        pred_vecmaps[idx + 1], util.to_variable(vecmap_angles[0], True, False)
+                    )
+                for idx, pred_vecmap in enumerate(pred_vecmaps[-2:]):
+                    loss2 += angle_loss(
+                        pred_vecmap, util.to_variable(vecmap_angles[idx + 1], True, False)
+                    )
+
+                outputs = outputs[-1]
+                pred_vecmaps = pred_vecmaps[-1]
+            else:
+                loss1 = road_loss(outputs, util.to_variable(labels[0], True, False), True)
+                loss2 = angle_loss(pred_vecmaps, util.to_variable(labels[0], True, False))
+
+            test_loss_iou += loss1.item()
+            test_loss_vec += loss2.item()
+
+            _, predicted = torch.max(outputs.data, 1)
+
+            correctLabel = labels[-1].view(-1, crop_size, crop_size).long()
+            hist += util.fast_hist(
+                predicted.view(predicted.size(0), -1).cpu().numpy(),
+                correctLabel.view(correctLabel.size(0), -1).cpu().numpy(),
+                config["task1_classes"],
+            )
+
+            _, predicted_angle = torch.max(pred_vecmaps.data, 1)
+            correct_angles = vecmap_angles[-1].view(-1, crop_size, crop_size).long()
+            hist_angles += util.fast_hist(
+                predicted_angle.view(predicted_angle.size(0), -1).cpu().numpy(),
+                correct_angles.view(correct_angles.size(0), -1).cpu().numpy(),
+                config["task2_classes"],
+            )
+
+            p_accu, miou, road_iou, fwacc = util.performMetrics(
+                train_loss_file,
+                val_loss_file,
+                epoch,
+                hist,
+                test_loss_iou / (i + 1),
+                test_loss_vec / (i + 1),
+                is_train=False,
+            )
+            p_accu_angle, miou_angle, fwacc_angle = util.performAngleMetrics(
+                train_loss_angle_file, val_loss_angle_file, epoch, hist_angles, is_train=False
+            )
+
+            viz_util.progress_bar(
+                i,
+                len(val_loader),
+                "Loss: %.6f | VecLoss: %.6f | road miou: %.4f%%(%.4f%%) | angle miou: %.4f%%"
+                % (
+                    test_loss_iou / (i + 1),
+                    test_loss_vec / (i + 1),
+                    miou,
+                    road_iou,
+                    miou_angle,
+                ),
+            )
+
+            if i % 100 == 0 or i == len(val_loader) - 1:
+                images_path = "{}/images/".format(experiment_dir)
+                util.ensure_dir(images_path)
+                util.savePredictedProb(
+                    inputsBGR.data.cpu(),
+                    labels[-1].cpu(),
+                    predicted.cpu(),
+                    F.softmax(outputs, dim=1).data.cpu()[:, 1, :, :],
+                    predicted_angle.cpu(),
+                    os.path.join(images_path, "validate_pair_{}_{}.png".format(epoch, i)),
+                    norm_type=config["val_dataset"]["normalize_type"],
                 )
-            for idx, pred_vecmap in enumerate(pred_vecmaps[-2:]):
-                loss2 += angle_loss(
-                    pred_vecmap, util.to_variable(vecmap_angles[idx + 1], True, False)
-                )
 
-            outputs = outputs[-1]
-            pred_vecmaps = pred_vecmaps[-1]
-        else:
-            loss1 = road_loss(outputs, util.to_variable(labels[0], True, False), True)
-            loss2 = angle_loss(pred_vecmaps, util.to_variable(labels[0], True, False))
+            del inputsBGR, labels, predicted, outputs, pred_vecmaps, predicted_angle
 
-        test_loss_iou += loss1.item()
-        test_loss_vec += loss2.item()
-
-        _, predicted = torch.max(outputs.data, 1)
-
-        correctLabel = labels[-1].view(-1, crop_size, crop_size).long()
-        hist += util.fast_hist(
-            predicted.view(predicted.size(0), -1).cpu().numpy(),
-            correctLabel.view(correctLabel.size(0), -1).cpu().numpy(),
-            config["task1_classes"],
-        )
-
-        _, predicted_angle = torch.max(pred_vecmaps.data, 1)
-        correct_angles = vecmap_angles[-1].view(-1, crop_size, crop_size).long()
-        hist_angles += util.fast_hist(
-            predicted_angle.view(predicted_angle.size(0), -1).cpu().numpy(),
-            correct_angles.view(correct_angles.size(0), -1).cpu().numpy(),
-            config["task2_classes"],
-        )
-
-        p_accu, miou, road_iou, fwacc = util.performMetrics(
+        accuracy, miou, road_iou, fwacc = util.performMetrics(
             train_loss_file,
             val_loss_file,
             epoch,
             hist,
-            test_loss_iou / (i + 1),
-            test_loss_vec / (i + 1),
+            test_loss_iou / len(val_loader),
+            test_loss_vec / len(val_loader),
             is_train=False,
+            write=True,
         )
-        p_accu_angle, miou_angle, fwacc_angle = util.performAngleMetrics(
-            train_loss_angle_file, val_loss_angle_file, epoch, hist_angles, is_train=False
-        )
-
-        viz_util.progress_bar(
-            i,
-            len(val_loader),
-            "Loss: %.6f | VecLoss: %.6f | road miou: %.4f%%(%.4f%%) | angle miou: %.4f%%"
-            % (
-                test_loss_iou / (i + 1),
-                test_loss_vec / (i + 1),
-                miou,
-                road_iou,
-                miou_angle,
-            ),
+        util.performAngleMetrics(
+            train_loss_angle_file,
+            val_loss_angle_file,
+            epoch,
+            hist_angles,
+            is_train=False,
+            write=True,
         )
 
-        if i % 100 == 0 or i == len(val_loader) - 1:
-            images_path = "{}/images/".format(experiment_dir)
-            util.ensure_dir(images_path)
-            util.savePredictedProb(
-                inputsBGR.data.cpu(),
-                labels[-1].cpu(),
-                predicted.cpu(),
-                F.softmax(outputs, dim=1).data.cpu()[:, 1, :, :],
-                predicted_angle.cpu(),
-                os.path.join(images_path, "validate_pair_{}_{}.png".format(epoch, i)),
-                norm_type=config["val_dataset"]["normalize_type"],
-            )
-
-        del inputsBGR, labels, predicted, outputs, pred_vecmaps, predicted_angle
-
-    accuracy, miou, road_iou, fwacc = util.performMetrics(
-        train_loss_file,
-        val_loss_file,
-        epoch,
-        hist,
-        test_loss_iou / len(val_loader),
-        test_loss_vec / len(val_loader),
-        is_train=False,
-        write=True,
-    )
-    util.performAngleMetrics(
-        train_loss_angle_file,
-        val_loss_angle_file,
-        epoch,
-        hist_angles,
-        is_train=False,
-        write=True,
-    )
-
-    if miou > best_miou:
-        best_accuracy = accuracy
-        best_miou = miou
-        util.save_checkpoint(epoch, test_loss_iou / len(val_loader), model, optimizer, best_accuracy, best_miou, config, experiment_dir)
+        if miou > best_miou:
+            best_accuracy = accuracy
+            best_miou = miou
+            util.save_checkpoint(epoch, test_loss_iou / len(val_loader), model, optimizer, best_accuracy, best_miou, config, experiment_dir)
 
     return test_loss_iou / len(val_loader)
 
